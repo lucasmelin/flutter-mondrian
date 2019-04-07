1f7cf55d83109cbddcebf05a634edfbb91793928
Lucas Melin <lucas.melin@gmail.com>
2019-04-07 19:34:00 -0400

    Generate either a line, triangle or square in a single quadrant.

--- lib/main.dart
+++ lib/main.dart
@@ -60,6 +60,78 @@ class _SquareCanvasPainter extends CustomPainter {
         new Paint()
           ..color = color
           ..style = PaintingStyle.fill);
+    canvas.drawPath(
+        path,
+        new Paint()
+          ..color = Colors.black
+          ..style = PaintingStyle.stroke
+          ..strokeWidth = 3.0);
+  }
+
+  @override
+  bool hitTest(Offset position) {
+    return path.contains(position);
+  }
+}
+
+
+class _SquareQuadrantPainter extends CustomPainter {
+  double padding;
+  Color color;
+
+  Path path;
+  double deviceWidth;
+
+  _SquareQuadrantPainter(context, segments, this.padding, this.color) {
+    double width = MediaQuery.of(context).size.width - padding;
+    double height = MediaQuery.of(context).size.height - padding;
+    double onePad = padding / 2;
+
+    double sideLength = min(width, height);
+    double startY = ((height - sideLength) / 2) + onePad;
+    double startX = ((width - sideLength) / 2) + onePad;
+
+    double centerX = (MediaQuery.of(context).size.width) / 2;
+    double centerY = (MediaQuery.of(context).size.height) / 2;
+
+    path = new Path();
+    path.moveTo(centerX, centerY);
+    if (segments > 0){
+      path.lineTo(centerX, startY + sideLength);
+    }
+    if (segments > 2){
+     path.lineTo(centerX, startY + sideLength);
+     path.lineTo(startX, startY + sideLength);
+    }
+    if (segments > 3){
+      path.lineTo(startX, centerY);
+    }
+    path.close();
+  }
+
+  @override
+  bool shouldRepaint(_SquareQuadrantPainter oldDelegate) {
+    return oldDelegate.deviceWidth != deviceWidth ||
+        oldDelegate.padding != padding ||
+        oldDelegate.color != color;
+  }
+
+  @override
+  bool shouldRebuildSemantics(_SquareQuadrantPainter oldDelegate) => true;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    canvas.drawPath(
+        path,
+        new Paint()
+          ..color = color
+          ..style = PaintingStyle.fill);
+    canvas.drawPath(
+        path,
+        new Paint()
+          ..color = Colors.black
+          ..style = PaintingStyle.stroke
+          ..strokeWidth = 3.0);
   }
 
   @override
@@ -81,7 +153,7 @@ class MondrianWidget extends StatelessWidget {
             painter: new _SquareCanvasPainter(context, 20.0, Colors.white),
           ),
           new CustomPaint(
-            painter: new _SquareCanvasPainter(context, 40.0, Colors.blue),
+            painter: new _SquareQuadrantPainter(context, 4, 20.0, Colors.blue),
           )
         ]),
       ),
